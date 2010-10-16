3b95a8285b930765cfd3e28e7b7824328de29f72
Haipeng Fan <haipeng@localhost.(none)>
2010-10-16 12:20:42 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goodswill::Application.config.secret_token = '32d18d3984632e201f17092a1f8790524eebc7f0fc1dbff958b3722af7c67fd75d2e9215d5a63af8697a87df25f56f8e36bb79dfff78af0468b875653865f8e0'
