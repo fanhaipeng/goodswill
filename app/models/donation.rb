deb9a267461c3889e70c5c9dcd19b23113085b38
Haipeng Fan <fanhaipeng@gmail.com>
2010-12-06 10:57:24 +0800

    donation model validates and unit tests

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,2 +1,10 @@
 class Donation < ActiveRecord::Base
+  validates_presence_of :phone, :address, :email, :name
+  validates_format_of :phone, 
+                      :with => /^1\d{10}$|^\d{2,4}-\d{7,8}$/,
+                      :message => 'the phone number is invalid'
+  validates_format_of :email,
+                      :with => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                      :message => 'email address is invalid'
+
 end
--- test/unit/donation_test.rb
+++ test/unit/donation_test.rb
@@ -1,8 +1,86 @@
 require 'test_helper'
 
 class DonationTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  test "fields except track and details must not be blank." do
+    donation = Donation.new
+    assert !donation.valid?
+
+    assert donation.errors[:phone].any?
+    assert donation.errors[:address].any?
+    assert donation.errors[:email].any?
+    assert donation.errors[:name].any?
+  end
+
+  test "phone must follow cell number rule" do
+    donation = Donation.new(
+      :address => 'any address',
+      :email => 'name@example.com',
+      :name => 'Tom White')
+    
+    err_msg = "the phone number is invalid"
+
+    # cell phone number, start with 1 & has 11 digits
+    donation.phone = '1381234567'
+    assert !donation.valid?
+    assert_equal err_msg, donation.errors[:phone].to_s
+
+    donation.phone = '138123456789'
+    assert !donation.valid?
+    assert_equal err_msg, donation.errors[:phone].to_s
+
+    donation.phone = '23812345678'
+    assert !donation.valid?
+    assert_equal err_msg, donation.errors[:phone].to_s
+
+    donation.phone = '13811189153'
+    assert donation.valid?
+
+    # telephone, follows area-number, area has 2 to 4 digits
+    # and number has 7 or 8 digits
+    donation.phone = '12345-1234567'
+    assert !donation.valid?
+    assert_equal err_msg, donation.errors[:phone].to_s
+
+    donation.phone = '1-1234567'
+    assert !donation.valid?
+    assert_equal err_msg, donation.errors[:phone].to_s
+
+    donation.phone = '12-1234567'
+    assert donation.valid?
+
+    donation.phone = '1234-1234567'
+    assert donation.valid?
+
+    donation.phone = '1234-123456'
+    assert !donation.valid?
+    assert_equal err_msg, donation.errors[:phone].to_s
+
+    donation.phone = '1234-123456789'
+    assert !donation.valid?
+    assert_equal err_msg, donation.errors[:phone].to_s
+
+    donation.phone = '1234-12345678'
+    assert donation.valid?
+
+    donation.phone = '1234-abc1234'
+    assert !donation.valid?
+    assert_equal err_msg, donation.errors[:phone].to_s
+  end
+
+  test "email address must be valid" do
+    donation = Donation.new(
+      :address => 'any address',
+      :phone => '010-59173532',
+      :name => 'Tom White')
+
+    err_msg = "email address is invalid"
+
+    donation.email = 'abcd'
+    assert !donation.valid?
+    assert_equal err_msg, donation.errors[:email].to_s
+
+    donation.email = 'goodswill@live.com'
+    assert donation.valid?
   end
 end
