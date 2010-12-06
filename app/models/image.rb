0b7e6c2956118ce6919420e7c8ec81b86421270d
Haipeng Fan <fanhaipeng@gmail.com>
2010-12-06 20:09:13 +0800

    images model and unit tests

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,8 @@
 class Image < ActiveRecord::Base
+  # image types
+  JPEG = 'images/jpeg'
+  PNG = 'images/png'
+
+  validates_presence_of :image_type, :data, :description
+  validates_format_of :image_type, :with => Regexp.new(JPEG << "|" << PNG)
 end
--- test/unit/image_test.rb
+++ test/unit/image_test.rb
@@ -1,8 +1,34 @@
 require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  test "fields must not be blank" do
+    img = Image.new
+    assert !img.valid?
+
+    assert img.errors[:image_type].any?
+    assert img.errors[:data].any?
+    assert img.errors[:description].any?
   end
+
+  test "only are jpeg and png supported" do
+    img = Image.new(
+      :image_type => Image::JPEG,
+      :data => [0xa, 0xb],
+      :description => 'any text')
+    assert img.valid?
+
+    img = Image.new(
+      :image_type => Image::PNG,
+      :data => [0xa, 0xb],
+      :description => 'any text')
+    assert img.valid?
+
+    img = Image.new(
+      :image_type => 'images/bmp',
+      :data => [0xa, 0xb],
+      :description => 'any text')
+    assert !img.valid?
+  end
+
 end
