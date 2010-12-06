c1b1fd0c3d77b9a11459561fbfa963190e405e7b
Haipeng Fan <fanhaipeng@gmail.com>
2010-12-06 15:16:52 +0800

    refactory item_test.rb

--- test/unit/item_test.rb
+++ test/unit/item_test.rb
@@ -20,11 +20,7 @@ class ItemTest < ActiveSupport::TestCase
   end
 
   test "status must be 0 to 4" do
-    item = Item.new(
-      :donation_id => 0,
-      :category => 'clothe',
-      :quantity => 1)
-    assert item.valid?
+    item = create_valid_item
 
     item.status = -1
     assert !item.valid?
@@ -40,24 +36,25 @@ class ItemTest < ActiveSupport::TestCase
   end
 
   test "item belongs to a donation" do
-    item = Item.new(
-      :donation_id => 0,
-      :category => 'clothe',
-      :quantity => 1)
-    assert item.valid?
-
+    item = create_valid_item
     item.donation = Donation.new
     assert item.donation
   end
 
   test "item belongs to a delivery" do
+    item = create_valid_item
+    item.delivery = Delivery.new
+    assert item.delivery
+  end
+
+  private
+
+  def create_valid_item
     item = Item.new(
       :donation_id => 0,
       :category => 'clothe',
       :quantity => 1)
     assert item.valid?
-
-    item.delivery = Delivery.new
-    assert item.delivery
+    item
   end
 end
