32389d7021ed7d1047f450db0e2875a1d4c19526
Haipeng Fan <fanhaipeng@gmail.com>
2010-12-13 18:20:07 +0800

    add validation to donation image model and unit tests

--- app/models/donation_image.rb
+++ app/models/donation_image.rb
@@ -1,3 +1,5 @@
 class DonationImage < ActiveRecord::Base
   belongs_to :donation
+
+  validates_presence_of :image_type, :data, :description
 end
--- test/unit/donation_image_test.rb
+++ test/unit/donation_image_test.rb
@@ -3,6 +3,10 @@ require 'test_helper'
 class DonationImageTest < ActiveSupport::TestCase
 
   test "fields must not be blank" do
-    dimg 
+    dimg = DonationImage.new
+    assert !dimg.valid?
+    assert dimg.errors[:image_type].any?
+    assert dimg.errors[:data].any?
+    assert dimg.errors[:description].any?
   end
 end
