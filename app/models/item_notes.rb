8fae015be8d7eba787472c6b1d419e24d4f18638
Haipeng Fan <fanhaipeng@gmail.com>
2010-12-13 18:01:15 +0800

    add item notes model and unit tests

--- app/models/item_notes.rb
+++ app/models/item_notes.rb
@@ -1,3 +1,6 @@
 class ItemNotes < ActiveRecord::Base
+  belongs_to :user
   has_and_belongs_to_many :items
+  validates_presence_of :user_id
+  validates_inclusion_of :status, :in => [Item::INITIAL, Item::PICKED, Item::SORTED, Item::WITHDREW, Item::DISPOSED]
 end
---
+++ test/unit/item_notes_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+class ItemNotesTest < ActiveSupport::TestCase
+
+  fixtures :users
+
+  test "user_id must not be blank" do
+    item_note = ItemNotes.new
+    assert !item_note.valid?
+  end
+
+  test "item note has many items" do
+    item_note = create_valid_note 
+    assert item_note.items
+  end
+
+  test "item note has the same status as item" do
+    item_note = create_valid_note
+    item_note.status = -1
+    assert !item_note.valid?
+    item_note.status = 0 
+    assert item_note.valid?
+    item_note.status = 4 
+    assert item_note.valid?
+    item_note.status = 5
+    assert !item_note.valid?
+  end
+
+  private
+
+  def create_valid_note
+    item_note = ItemNotes.new
+    item_note.user = users(:user_one)
+    assert item_note.valid?
+    item_note
+  end
+end
