65223eec86b1a9732e37c223e7fc09c02e9e4b65
Haipeng Fan <fanhaipeng@gmail.com>
2010-12-28 19:28:30 +0800

    user <%= for form_for; add functional tests in pick_up controller for authentication

--- test/functional/pick_up_controller_test.rb
+++ test/functional/pick_up_controller_test.rb
@@ -2,16 +2,28 @@ require 'test_helper'
 
 class PickUpControllerTest < ActionController::TestCase
   test "index of pickup should be ok" do
+    session[:user_id] = users(:user_two)
     get :index
     assert_response :success
     assert assigns(:donations)
   end
 
+  test "anonymous user can't see index" do
+    get :index
+    assert_redirected_to account_login_path
+  end
+
   test "show of pickup should be ok" do
+    session[:user_id] = users(:user_two)
     get :show, :id => donations(:donation_one)
     assert_response :success
     assert assigns(:donation)
     assert assigns(:donation_notes)
+  end
+
+  test "anonymous user can't see show" do
+    get :show, :id => donations(:donation_one)
+    assert_redirected_to account_login_path
   end
 
   test "update status should be ok" do
@@ -20,5 +32,10 @@ class PickUpControllerTest < ActionController::TestCase
       put :update, :id => donations(:donation_one).id.to_s, :donation => { :status => Donation::PICKED }, :donation_note => { :note => "picked up by ourselves" }
     end
     assert_redirected_to pick_up_index_path
+  end
+
+  test "anonymous user can't do update" do
+    put :update, :id => donations(:donation_one).id.to_s, :donation => { :status => Donation::PICKED }, :donation_note => { :note => "picked up by ourselves" }
+    assert_redirected_to account_login_path
   end
 end
