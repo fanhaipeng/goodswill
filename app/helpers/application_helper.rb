8b00e6c13a5678684e12855f25ec1c2f4ee6a15f
Haipeng Fan <fanhaipeng@gmail.com>
2010-12-31 04:55:00 +0800

    add application layout, css and other view-related stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def build_tab_bar(active_tab)
+    return_text = ""
+    { 
+      :home => { :url => home_index_path, :text => "Home" },
+      :introduction => { :url => "", :text => "Introduction" },
+      :donations => { :url => donation_query_path, :text => "Donations" },
+      :receivers => { :url => receivers_path, :text => "Receivers" },
+      :news => { :url => news_index_path, :text => "News" },
+      :admin => { :url => deliveries_path, :text => "Admin" }
+    }.each_pair do |k, v| 
+      if (not session[:user_id]) and k == :admin
+        next;
+      end
+
+      if active_tab == k
+        return_text << raw( "<li class=\"active_tab\">#{v[:text]}</li>")
+      else
+        return_text << raw("<li class=\"normal_tab\">#{link_to v[:text], v[:url]}</li>")
+      end
+    end
+    raw return_text
+  end
 end
