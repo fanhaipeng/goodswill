1262fd40de5321333ca893b4d6c0f1a40c50eb08
Haipeng Fan <fanhaipeng@gmail.com>
2011-02-01 15:26:01 +0800

    make error messages more friendly

--- app/models/receiver.rb
+++ app/models/receiver.rb
@@ -4,9 +4,9 @@ class Receiver < ActiveRecord::Base
   validates_presence_of :name, :introduction, :address, :phone, :email 
   validates_format_of :email, 
                       :with => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                      :message => 'email address is invalid'
+                      :message => 'format is invalid'
   validates_format_of :phone,
                       :with => /^1\d{10}$|^\d{2,4}-\d{7,8}$/,
-                      :message => 'the phone number is invalid'
+                      :message => 'format is invalid'
 
 end
