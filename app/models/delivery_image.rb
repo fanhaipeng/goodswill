172d4582e591238aad8247609f2277b2f00bb5d1
Haipeng Fan <fanhaipeng@gmail.com>
2011-02-01 14:59:23 +0800

    gracefully handle errors that the uploaded file is too big

--- app/models/delivery_image.rb
+++ app/models/delivery_image.rb
@@ -1,16 +1,19 @@
 class DeliveryImage < ActiveRecord::Base
   belongs_to :delivery
 
+  validate :validate_data
   validates_presence_of :image_type, :data
-
-  before_validation :transform_data
 
   attr_accessor :image_data
 
-  def transform_data
+  def validate_data
     if image_data
-      self.data = image_data.read
-      self.image_type = self.image_data.content_type.chomp
+      if self.image_data.size > 524288 # 512KB
+        errors.add(:data, "must be smaller than 512KB")
+      else
+        self.data = image_data.read
+        self.image_type = self.image_data.content_type.chomp
+      end
     end
   end
 end
