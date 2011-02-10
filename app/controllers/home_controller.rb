2cbc0a8655d59e0ee2cbb09bb7436ea997ae090e
Haipeng Fan <fanhaipeng@gmail.com>
2011-02-10 10:04:34 +0800

    bug fix: show latest three pieces of news on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @latest_news = News.all(:order => :updated_at, :limit => 3) 
-    @latest_items = Item.all(:order => :created_at, :limit => 5)
+    @latest_news = News.order("updated_at desc").limit(3)
     @sub_title = "Home"
   end
 end
