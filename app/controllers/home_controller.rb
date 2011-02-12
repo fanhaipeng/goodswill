8b0dd8b2558df22ec2589092d4d51e9822418400
Haipeng Fan <fanhaipeng@gmail.com>
2011-02-12 17:02:27 +0800

    update pictures, favicon, and fix bug of news order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @latest_news = News.order("updated_at desc").limit(3)
+    @latest_news = News.order("created_at desc").limit(3)
     @sub_title = "Home"
   end
 end
